29b96baf5e3fc2e3b597a4d693a2b9631f484094
Ahmed Ellash <ahmadellashy2000@gmail.com>
2024-06-12 02:33:27 +0300

    Initial Commit

---
+++ InstaUITests/InstaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InstaUITestsLaunchTests.swift
+//  InstaUITests
+//
+//  Created by Ahmad Ellashy on 12/06/2024.
+//
+
+import XCTest
+
+final class InstaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
